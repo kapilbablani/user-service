9ea31c315399983d7ae138d2e809546dac4dcc26
Kapil Bablani <bablanikapil@gmail.com>
2020-06-24 12:25:13 +0530

    config at common place

--- src/main/java/com/user/dao/impl/RoleDaoImpl.java
+++ src/main/java/com/user/dao/impl/RoleDaoImpl.java
@@ -6,17 +6,17 @@ import com.user.model.CreateRole;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.Transaction;
-import org.hibernate.cfg.Configuration;
-import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class RoleDaoImpl implements RoleDao {
 
+    @Autowired
+    private SessionFactory sessionFactory;
+
     @Override
     public String createRole(CreateRole role) {
-        Configuration configuration = new Configuration().configure().addAnnotatedClass(Role.class);
-        SessionFactory sessionFactory = configuration.buildSessionFactory();
         Session session =  sessionFactory.openSession();
         Transaction transaction = session.beginTransaction();
         Role roleToCreate = new Role();
